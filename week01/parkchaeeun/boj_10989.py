dc1c8f8200b8f2d36170ea4d51bf16732e2f4ee6
park chaeeun <tab0099@duksung.ac.kr>
2025-05-06 01:14:41 +0900

    boj_10989_수 정렬하기 3

---
+++ week01/parkchaeeun/boj_10989.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+count = [0] * (10000+1)
+
+n = int(input())
+for _ in range(n):
+    a = int(input())
+    count[a] += 1
+for i in range(len(count)):
+    for _ in range(count[i]):
+        print(i)
+import sys
+
